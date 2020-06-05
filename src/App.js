d9a1ecc278e2adc5f831294e74cd23703615ca5b
unknown <oubaniayoub@gmail.com>
2020-06-05 19:47:39 +0100

    update app file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 import "./App.css";
 class App extends Component {
   render() {
-    return <div> Github users finder </div>;
+    return (
+      <div>
+        <h1>Hello world</h1>
+      </div>
+    );
   }
 }
 

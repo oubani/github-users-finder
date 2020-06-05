d167d0519a28b29316235a2cce97a0c8afe7446f
unknown <oubaniayoub@gmail.com>
2020-06-05 19:42:04 +0100

    init fproject

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import "./App.css";
+class App extends Component {
+  render() {
+    return <div> Github users finder </div>;
+  }
+}
+
+export default App;
